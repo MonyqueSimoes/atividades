7dc63c869032273701a947b17dff40e12f404c57
MonyqueSimoes <monyquesimoes@hotmail.com>
2013-12-18 16:27:50 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,16 @@
+<?php
+$x = 9;
+$y = 12;
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "O dia de hoje é ";
+
+echo $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "O dia de hoje é $dia/$mes/$ano"; 
+echo "<br/>";
+echo "$x + $y = " . ($x + $y); 
+
+?>
