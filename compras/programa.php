fd79a628776f00475a0f01d0f11d3cebbe759203
MonyqueSimoes <monyquesimoes@hotmail.com>
2013-12-18 16:17:27 -0200

    Create programa.php

---
+++ compras/programa.php
@@ -0,0 +1,28 @@
+<?php
+
+$textoInicio = $_POST["inicio"];
+$textoFim = $_POST["fim"];
+
+$textoInicio = explode ("/", $textoInicio);
+$textoFim =  explode ("/", $textoFim);
+
+$tempoInicio = mktime (0, 0, 0, $textoInicio[1], $textoInicio[0], $textoInicio[2]);
+$tempoFim = mktime (0, 0, 0, $textoFim[1], $textoFim[0], $textoFim[2]);
+
+	$tempoAtual = time();
+
+echo "Hoje é um bom dia para compras?";
+
+if ($tempoAtual >= $tempoInicio And $tempoAtual <= $tempoFim)
+{
+
+	echo "sim";
+}
+
+else{
+
+	echo "não";
+
+}
+
+?>
